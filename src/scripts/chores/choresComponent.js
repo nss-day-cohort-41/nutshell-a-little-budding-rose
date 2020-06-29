ec39722c019bc91f3e8fc89969d8ddc59bb85706
Lacey Walker <65372269+laceywalkerr@users.noreply.github.com>
2020-06-29 14:25:45 -0500

    no errors anymore i forgot s at the end of chores in url

--- src/scripts/chores/choresComponent.js
+++ src/scripts/chores/choresComponent.js
@@ -4,12 +4,12 @@ const url = "http://localhost:8088";
 
 const API = {
     getAllChores: () => {
-        return fetch(`${url}/chore`)
+        return fetch(`${url}/chores`)
         .then(response => response.json());
         
     },
     addAChore: (choreObject) => {
-        return fetch(`${url}/chore`, {
+        return fetch(`${url}/chores`, {
             method: "POST",
             headers: {
                     "contentType": "application/json"

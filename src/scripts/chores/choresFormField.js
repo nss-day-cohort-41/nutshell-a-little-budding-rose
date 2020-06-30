7df8fef957356480a7c6718d51b98ba2f3303aab
Lacey Walker <65372269+laceywalkerr@users.noreply.github.com>
2020-06-30 15:54:44 -0500

    working on adding formfield

---
+++ src/scripts/chores/choresFormField.js
@@ -0,0 +1,5 @@
+const choresFormField = (choreObject) => {
+    const hiddenChoreId = document.querySelector("#id");
+    const choreNameInput = document.querySelector("#name");
+    const choreDateInput = document.querySelector("#date");
+}

165e288dce429cc97386cc5b5c9881a997a58576
chrismccolgan <mccolganchrisj@gmail.com>
2020-06-30 15:42:52 -0500

    got news working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,14 @@ import messageList from "./messages/messagesList.js"
 import makeChore from './chores/choresList.js';
 import messageEventListener from "./messages/messageEvents.js"
 import userButtons from "./users/usersList.js"
+import  { newsButtons, showNewsEntries } from "./news/newsList.js"
 
 userButtons.logIn()
 userButtons.register()
+
+showNewsEntries()
+newsButtons.save()
+newsButtons.deleteEdit()
 
 messagesAPI.messagesGetData()
 .then(() => {
